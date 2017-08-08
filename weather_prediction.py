e718df844ab8174f68416ea5279cc1371de2032a
pac6 <pac6@sfu.ca>
2017-08-07 22:41:23 -0700

    Add filename list

--- weather_prediction.py
+++ weather_prediction.py
@@ -1,8 +1,19 @@
 # pylint: disable=missing-docstring, invalid-name, line-too-long
+"""
+header
+"""
+import sys
+import pandas as pd
+from scipy import misc
 
 def main():
-    print('hello world')
+    in_data_fp = sys.argv[1]
+    in_img_fp = sys.argv[2]
+
+    data = pd.read_csv(in_data_fp)
+    imglist = data['filename']
+    print(imglist)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
