c1bdf1c670c0ec02ba0a5cbf5b40e97b746bb1ca
Patrick Cheung <patrickcheungdev@gmail.com>
2017-08-08 18:49:11 -0700

    Add using pixels as features

--- weather_prediction.py
+++ weather_prediction.py
@@ -6,8 +6,11 @@ observation data and images.
 Outputs analysis graphs.
 """
 import sys
+import numpy as np
 import pandas as pd
 from scipy import misc
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 """
 Returns images as a 1-d array
@@ -24,6 +27,13 @@ def get_images(path, filenames):
     return imgs
 
 
+"""
+Returns array that keeps every 16th pixel.
+"""
+def reduce_pixels(col):
+    return col[1::16]
+
+
 def main():
     in_data_fp = sys.argv[1]
     in_img_fp = sys.argv[2]
@@ -32,9 +42,36 @@ def main():
     data = pd.read_csv(in_data_fp)
     imglist = data['filename']
     imgs = get_images(in_img_fp, imglist)
-
     data_imgs = pd.merge(data, imgs, how='inner', on='filename', sort=False)
-    print(data_imgs.head(5))
+
+    # Reducing image feature points.
+    # Too many features causing out of memory.
+    # Adapted from: https://stackoverflow.com/a/17777520
+    data_imgs['img'] = data_imgs['img'].apply(reduce_pixels)
+
+    # Exploding flattened 2d image.
+    # Adapted from: https://stackoverflow.com/q/32468402
+    img_pts = pd.DataFrame(data_imgs['img'].values.tolist())
+    data_imgs_pts = data_imgs.join(img_pts)
+
+    # Hard coding 9216 image points from keeping every 16th pixel.
+    int_list = list(range(0, 9216))
+    #str_list = [str(x) for x in int_list]
+
+    features = [
+        'Temp (C)', 'Dew Point Temp (C)', 'Rel Hum (%)', 'Wind Dir (10s deg)', 'Wind Spd (km/h)',
+        'Visibility (km)', 'Stn Press (kPa)'#, 'img'
+    ]# + str_list
+
+    X = data_imgs_pts[features].values
+    y = data_imgs_pts['Weather'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    model = KNeighborsClassifier(n_neighbors=45)
+    model.fit(X_train, y_train)
+    print(model.score(X_test, y_test))
+    df = pd.DataFrame({'truth': y_test, 'prediction': model.predict(X_test)})
+    print(df[df['truth'] != df['prediction']])
 
 
 if __name__ == '__main__':
